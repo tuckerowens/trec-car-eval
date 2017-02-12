d4d56ccaa51329e722b4e8d2b47501149386f33d
Bahram Behzadian <zodiac_se@yahpp.com>
2017-02-12 18:56:16 -0500

    add  Precision@%d and recall@%d and Precision@R

--- trec-eval/src/main/java/edu/unh/cs/trec/metrics.java
+++ trec-eval/src/main/java/edu/unh/cs/trec/metrics.java
@@ -26,23 +26,42 @@ public class metrics {
 	public  ArrayList<Double>metric_methods(int map_k,int pr_k){
 		ArrayList<Double> eval = new ArrayList<Double>();
 		double MAP_a_q=0.0;
-		int counter=1;
+
+
+		double precisionAtX=0.0;
+		double recallAtX=0.0;
+		double precisionAtR=0.0;
+
+
+		int data_patecounter=1;
 		int num_of_true = truth_rel.size();
 
 		int ranklist_higherbound = Math.max(num_of_true, Math.max(map_k,pr_k));
 		for(String docId: ranklist.subList(0, ranklist_higherbound)){
-			if(truth_rel.contains(docId)){
-				if(counter<map_k){
-					MAP_a_q += 1.0/(1+ranklist.indexOf(docId));}
-				counter+=1;
-//				System.out.println(MAP_a_q);
-			}}
+                    if(truth_rel.contains(docId)){
+                        if(counter<map_k){
+                            MAP_a_q += 1.0/(1+ranklist.indexOf(docId));
+                        }
+                        if(counter<pr_k){
+                            precisionAtX+=1.0;
+                            recallAtX+=1.0;
+                        }
+                        if(counter<=num_of_true){
+                            precisionAtR+=1.0;
+                        }
+                        counter+=1;
+                    }
+		}
 		eval.add(MAP_a_q/num_of_true);
 
 		return eval;
 	}
 	public  ArrayList <Double> getResult(){
 		System.out.printf("MAP@%d is "+this.result.get(0)+"\n",this.map_k);
+                // add Precision  @R, recall@%d
+		System.out.printf("Precision@%d is "+this.result.get(1)+"\n",this.pr_k);
+		System.out.printf("recall@%d is "+this.result.get(2)+"\n",this.pr_k);
+		System.out.printf("Precision@R is "+this.result.get(3)+"\n");
 		return this.result;
 	}
 
