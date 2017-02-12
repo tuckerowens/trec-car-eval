18be1cb07ea64a6eb4c452d5360bf3ded4a20b69
Hang Zhang <bryan.zhanghang@gmail.com>
2017-02-12 15:24:55 -0500

    variable name change queryid to sectionid in the retrievaltoy class

--- trec-eval/src/main/java/edu/unh/cs/trec/retrievalToy.java
+++ trec-eval/src/main/java/edu/unh/cs/trec/retrievalToy.java
@@ -1,4 +1,4 @@
-package edu.unh.cs.trec;
+package evaluation;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -14,53 +14,57 @@ import java.util.Map;
 import java.util.Set;
 
 public class retrievalToy {
+	 String data_path;
+	 String data_set;
 	 static Map<String, Set<String> >truth;
 	 static Set<String> truth_rel;
 	 static ArrayList<String> ranklist;
 //	@SuppressWarnings("static-access")
-	public retrievalToy(String filename) throws NumberFormatException, IOException{
+	public retrievalToy(String data_path, String data_set) throws NumberFormatException, IOException{
+		this.data_path=data_path;
+		this.data_set=data_set;	
 
-
-		ArrayList<String> documentsRanking = new ArrayList<String>();
+		ArrayList<String> documentsRanking = new ArrayList<String>();	
 		Map<String, Set<String> >groundtruth = new HashMap<String, Set<String>>();
 		Set<String> groundtruthRelOnly = new HashSet();
+		String filename = data_path+data_set;
 		String line;
 		InputStream is = new FileInputStream(new File(filename));
 		BufferedReader br = new BufferedReader(new InputStreamReader(is));
-
+		
 		while ((line =br.readLine()) != null){
-
+			
 				String[] parsedLine = line.split(" ");
 				String sectionId = parsedLine[0];
 				String docId = parsedLine[2];
 				documentsRanking.add(docId);
 				int relScore = Integer.valueOf(parsedLine[3]);
-
+				
 				if(relScore>0){
 					groundtruthRelOnly.add(docId);
 					if(groundtruth.containsKey(sectionId)){
 						groundtruth.get(sectionId).add(docId);
-					}else{
+					}else{ 
 						Set<String> trueDocId= new HashSet <String>();
 						trueDocId.add(docId);
-						groundtruth.put(queryId, trueDocId);}
-				}
+						groundtruth.put(sectionId, trueDocId);}
+				}			
 		}
-		br.close();
+		br.close();	
 		truth = groundtruth;
 		ranklist=documentsRanking;
 		truth_rel = groundtruthRelOnly;
 	}
-
+	
 	public  ArrayList<String> getRankList(){
-		return ranklist;
+		return ranklist;	
 	}
-
-
+	
+	
 	public  Map<String, Set<String>> getTruth(){
 		return truth;
 	}
-
+	
 	public Set<String> getTruthRel(){
 		return truth_rel;
 	}
